0dc7e0389b85b18c74c46bc2aada41ad716cc167
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-10-17 01:31:02 +1000

    Update Main.php

--- src/skyss0fly/FakeOP/Main.php
+++ src/skyss0fly/FakeOP/Main.php
@@ -2,34 +2,30 @@
 
 namespace skyss0fly\FakeOP;
 
-use pocketmine{plugin\PluginBase, command\Command, command\CommandSender};
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 class Main extends PluginBase {
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         switch ($command->getName()) {
             case "fakeop":
-                if(count($args) < 1) {
+                if (count($args) < 1) {
                     $sender->sendMessage("Usage: /fakeop <player>");
                     return false;
                 }
 
                 $player = $this->getServer()->getPlayer($args[0]);
-                if($player === null) {
+                if ($player === null) {
                     $sender->sendMessage("Player not found");
                     return false;
+                } else {
+                    $sender->sendMessage("You have FakeOP'd " . $player->getName());
+                    $player->sendMessage("[" . $sender->getName() . "] Opped " . $player->getName() . ".");
                 }
-                else {
-                $sender->sendMessage("You Have FakeOP'd " . $player->getName());
-                $player->sendMessage("[" . $sender->getName(); . "] Opped " . $player->getName(); ".");
-                }
-
-                }
-                }
-                }
-                
-
-
-
-
-             
+                return true; // Make sure to return true to indicate command success
+        }
+        return false; // If the command is not recognized, return false
+    }
+}
